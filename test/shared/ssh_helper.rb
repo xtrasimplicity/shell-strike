37122c0ebb4655b7e257e45a5296060e177d5b9b
XtraSimplicity <andrew@xtrasimplicity.com>
2018-05-12 15:41:11 +1000

    Removed `duck_type_including` matcher in favour of `an_object_having_attributes`.
    As per https://github.com/rspec/rspec-mocks/pull/1221#issuecomment-388389035

--- test/shared/ssh_helper.rb
+++ test/shared/ssh_helper.rb
@@ -1,12 +1,10 @@
-require_relative 'matchers/duck_type_including'
-
 module SSHHelper
   def mock_host_as_online(host, port)
-   allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(duck_type_including(host: host, port: port)).and_return([true, nil])
+   allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(an_object_having_attributes(host: host, port: port)).and_return([true, nil])
   end
 
   def mock_host_as_offline(host, port)
-    allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(duck_type_including(host: host, port: port)).and_return([false, :unreachable])
+    allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(an_object_having_attributes(host: host, port: port)).and_return([false, :unreachable])
   end
 
   def mock_all_hosts_as_offline
@@ -14,19 +12,19 @@ module SSHHelper
   end
 
   def mock_connection_timeout(host, port)
-    allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(duck_type_including(host: host, port: port)).and_return([false, :timeout])
+    allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(an_object_having_attributes(host: host, port: port)).and_return([false, :timeout])
   end
 
   def mock_unexpected_connection_failure(host, port)
-    allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(duck_type_including(host: host, port: port)).and_return([false, :unexpected_error])
+    allow(ShellStrike::Ssh).to receive(:check_host_reachable).with(an_object_having_attributes(host: host, port: port)).and_return([false, :unexpected_error])
   end
 
   def stub_valid_ssh_credentials(host, port, valid_credentials)
-    allow(ShellStrike::Ssh).to receive(:valid_credentials?).with(duck_type_including(host: host, port: port), anything, anything).and_return(false)
+    allow(ShellStrike::Ssh).to receive(:valid_credentials?).with(an_object_having_attributes(host: host, port: port), anything, anything).and_return(false)
 
     
     valid_credentials.each do |username, password|
-     allow(ShellStrike::Ssh).to receive(:valid_credentials?).with(duck_type_including(host: host, port: port), username, password).and_return(true)
+     allow(ShellStrike::Ssh).to receive(:valid_credentials?).with(an_object_having_attributes(host: host, port: port), username, password).and_return(true)
     end
   end
 end
